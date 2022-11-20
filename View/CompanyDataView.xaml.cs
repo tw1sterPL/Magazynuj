9779afaa95fa999c691ce106f2fd8942daef09e2
tw1sterPL <gorslukasz@gmail.com>
2022-11-20 16:20:57 +0100

    addn new view

--- View/CompanyDataView.xaml.cs
+++ View/CompanyDataView.xaml.cs
@@ -24,9 +24,5 @@ namespace Magazynuj.View
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
